a797860d9481ddc49728f69d7f96e771fb692de6
Biswanath Tewari <biswanath@geekyants.com>
2021-10-06 17:33:01 +0530

    fix - tab bar ui improvement

--- src/components/AppTabBar.js
+++ src/components/AppTabBar.js
@@ -17,7 +17,11 @@ const Apptabbar = (props) => {
 
   const HomeLeftButton = () => (
     <TouchableOpacity>
-      <MaterialCommunityIcons name="theme-light-dark" size={24} color="white" />
+      <MaterialCommunityIcons
+        name="theme-light-dark"
+        size={24}
+        color={color.sky_blue}
+      />
     </TouchableOpacity>
   );
 
@@ -29,7 +33,11 @@ const Apptabbar = (props) => {
       <Text style={{ color: "white", position: "absolute", right: "100%" }}>
         My Feed
       </Text>
-      <MaterialIcons name="arrow-forward-ios" size={24} color="white" />
+      <MaterialIcons
+        name="arrow-forward-ios"
+        size={24}
+        color={color.sky_blue}
+      />
     </TouchableOpacity>
   );
 
@@ -38,7 +46,7 @@ const Apptabbar = (props) => {
       style={styles.buttonContainer}
       onPress={() => jumpTo("home")}
     >
-      <MaterialIcons name="arrow-back-ios" size={24} color="white" />
+      <MaterialIcons name="arrow-back-ios" size={24} color={color.sky_blue} />
       <Text style={{ color: "white", position: "absolute", left: "100%" }}>
         Discover
       </Text>
@@ -47,16 +55,18 @@ const Apptabbar = (props) => {
 
   const FeedRightButton = () => (
     <TouchableOpacity onPress={() => getNews({ isFromFeed: true })}>
-      <Ionicons name="ios-reload" size={24} color="white" />
+      <Ionicons name="ios-reload" size={24} color={color.sky_blue} />
     </TouchableOpacity>
   );
 
   return (
     <View style={[styles.container, { backgroundColor: color.dark_secondary }]}>
       {index === 0 ? <HomeLeftButton /> : <FeedLeftButton />}
-      <Text style={{ color: "white", textDecorationLine: "underline" }}>
-        {index === 0 ? "Discover" : "My Feed"}
-      </Text>
+      <View style={styles.titleWrapper}>
+        <Text style={[styles.title, { color: "white" }]}>
+          {index === 0 ? "Discover" : "My Feed"}
+        </Text>
+      </View>
       {index === 0 ? <HomeRightButton /> : <FeedRightButton />}
     </View>
   );
@@ -70,6 +80,16 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     paddingHorizontal: 8,
+  },
+  titleWrapper: {
+    borderBottomWidth: 2,
+    borderBottomColor: color.sky_blue,
+    paddingBottom: 5,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: "800",
   },
   buttonContainer: {
     flexDirection: "row",
--- src/config/color.js
+++ src/config/color.js
@@ -1,4 +1,5 @@
 export default {
   dark_primary: "#17223B",
   dark_secondary: "#263859",
+  sky_blue: "#87CEEB",
 };
